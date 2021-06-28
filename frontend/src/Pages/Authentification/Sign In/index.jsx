726a2775cf9b6da3d5596814deff4090392ac746
laptop <clerc.fabien@bluewin.ch>
2021-06-28 17:59:32 +0200

    add base button and input

--- frontend/src/Pages/Authentification/Sign In/index.jsx
+++ frontend/src/Pages/Authentification/Sign In/index.jsx
@@ -1,7 +1,39 @@
+import styled from "styled-components";
+import {BaseInput} from "../../../Components/Generic/Fields";
+import {BaseButton} from "../../../Components/Generic/Buttons";
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    height: 100vh;
+    width: 100%;
+    background-color: ${props => props.theme.backgroundLightGrey};
+`
+
+const Title = styled.h1`
+    color: ${props => props.theme.textDarkGrey};
+    margin-top: 5%;
+    margin-bottom: 7%;
+`
+
+const RegistrationInput = styled(BaseInput)`
+    width: 30%;
+    min-width: 200px;
+    font-size: ${props => props.theme.textSizeM};
+    padding: 23px;
+    margin-bottom: 5%;
+`
+
 const SignIn = () => {
     return (
-        <h1>Sign In!</h1>
+        <Wrapper>
+            <Title>REGISTRATION</Title>
+            <RegistrationInput name={"E-Mail address"} />
+            <BaseButton name="Register" >Register</BaseButton>
+        </Wrapper>
     )
 }
 
-export default SignIn
+export default SignIn;
--- frontend/src/Styles/index.js
+++ frontend/src/Styles/index.js
@@ -13,53 +13,34 @@ export const GlobalStyle = createGlobalStyle`
 `;
 
 export const defaultTheme = {
-  // Colors:
+  // Colors
   orange: "#E47D31",
   backgroundLightGrey: "#F2F2F2",
   backgroundWhite: "#FFFFFF",
-  TextTitleDarkGrey: "#4C4C4C",
-  TextTabDarkGrey: "#4A4A4A",
-  TextGrey: "#979797",
-  TextFooterGrey: "#646363",
-  TextProfileBlack: "#303030",
-  TextBlack: "#000000",
-  TextWhite: "#FFFFFF",
-  TextRating: "#7E7E7E",
-  separatorGrey: "#EBEBEB",
+  textDarkGrey: "#4C4C4C",
+  textTabDarkGrey: "#4A4A4A",
+  textGrey: "#979797",
+  textFooterGrey: "#646363",
+  textProfileBlack: "#303030",
+  textBlack: "#000000",
+  textWhite: "#FFFFFF",
+  textRating: "#7E7E7E",
+  DetailsGrey: "#EBEBEB",
 
-  // LINEAR GRADIENT
-  // linearGradientGradBackground: "linear-gradient(102deg, #c468ff, #6e91f6)",
-  // linearGradientGradButton: "linear-gradient(132.96deg, #C468FF 3.32%, #6E91F6 100%)",
+  // Linear Gradients
 
-  // Box Shadows:
-  boxShadowButton: "box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.07)",
-  boxShadowFriends: "box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.2), 0px 10px 20px rgba(0, 0, 0, 0.05)",
+  // Box Shadows
 
   // Sizes
-  // controlHeight: "40px",
-  // controlHeightMini: "24px",
-  // controlHeightSmall: "32px",
-  // controlHeightLarge: "48px",
-  // spaceXXS: "4px",
-  // spaceXS: "8px",
-  // spaceS: "16px",
-  // spaceM: "24px",
-  // spaceL: "32px",
-  // spaceXL: "48px",
-  // spaceXXL: "220px",
 
   // Text Size
-  // textSizeXXL: "40px",
-  // textSizeXL: "30px",
   textSizeL: "30px",
   textSizeM: "20px",
-  // textSizeTab: "20px",
   textSizeSecondTitle: "24px",
   textSizePostName: "18px",
   textSizeDefault: "16px",
   textSizeS: "14px",
   textSizeXS: "12px",
-  // textSizeXXS: "10px",
 
   // Text Weight
   textWeightThin: "300",
