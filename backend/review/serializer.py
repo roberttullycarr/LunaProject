aa5e13255fb7d49246b35b809dfb83c3231d8884
Valentina Sulbaran <vjss05@gmail.com>
2021-07-01 14:51:18 +0200

    restaurantNameDisplayedInReview

--- backend/review/serializer.py
+++ backend/review/serializer.py
@@ -7,6 +7,10 @@ class ReviewSerializer(serializers.ModelSerializer):
     user = UserProfileSerializerPublic(read_only=True)
     amount_of_comments_in_review = serializers.SerializerMethodField()
     amount_of_likes_in_review = serializers.SerializerMethodField()
+    restaurant_name = serializers.SerializerMethodField()
+
+    def get_restaurant_name(self, instance):
+        return instance.restaurant.name
 
     @staticmethod
     def get_amount_of_comments_in_review(instance):
@@ -19,5 +23,5 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = "__all__"
-        fields = ['user', 'text', 'rating', 'restaurant', 'created', 'modified', 'likes',
-                  'amount_of_comments_in_review', 'amount_of_likes_in_review']
+        fields = ['user', 'text', 'rating', 'restaurant', 'restaurant_name', 'created', 'modified',
+                  'amount_of_comments_in_review', 'amount_of_likes_in_review', 'likes']
