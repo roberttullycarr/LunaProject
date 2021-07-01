34c8c3328b8a001ecfd62b6a4566e182eefa671e
Valentina Sulbaran <vjss05@gmail.com>
2021-07-01 13:50:44 +0200

    amountOfEverythinginUserReviewRestaurant

--- backend/review/serializer.py
+++ backend/review/serializer.py
@@ -5,7 +5,19 @@ from user.serializer import UserProfileSerializerPublic
 
 class ReviewSerializer(serializers.ModelSerializer):
     user = UserProfileSerializerPublic(read_only=True)
+    amount_of_comments_in_review = serializers.SerializerMethodField()
+    amount_of_likes_in_review = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_amount_of_comments_in_review(instance):
+        return instance.comment_review.all().count()
+
+    @staticmethod
+    def get_amount_of_likes_in_review(instance):
+        return instance.likes.all().count()
 
     class Meta:
         model = Review
         fields = "__all__"
+        fields = ['user', 'text', 'rating', 'restaurant', 'created', 'modified', 'likes',
+                  'amount_of_comments_in_review', 'amount_of_likes_in_review']
