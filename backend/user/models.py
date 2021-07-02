1f9c4ad2071c124429de482b7819108d57dbbe48
robertcarr <roberttullycarr@gmail.com>
2021-07-02 15:53:40 +0200

    merge with dev

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,4 +1,3 @@
-<<<<<<< backend/user/models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from reg_profile.models import RegProfile
@@ -24,5 +23,4 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-=======
->>>>>>> backend/user/models.py
+
